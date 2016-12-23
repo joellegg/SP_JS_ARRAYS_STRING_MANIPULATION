4cf7e48ad1da209f4643b50ff7cb3758fcb06ede
joellegg <joellegg@bellsouth.net>
2016-12-23 10:54:08 -0600

    alphbits and palindrome functions working

--- strings.js
+++ strings.js
@@ -3,7 +3,10 @@
 ////////////////////////////
 
 let testString;
-//let joinArray;
+let joinArray;
+let alphaOrder;
+let palinYesOrNo;
+
 
 /////////////////////
 ///   Functions   ///
@@ -24,11 +27,11 @@ function getInput() {
 // reverses the order of the characters in the string, and outputs the result in the DOM, below the text input.
 function reversal() {
     // Step 1. Use the split() method to return a new array
-    var splitString = testString.split(""); // var splitString = "hello".split("");
+    let splitString = testString.split(""); // var splitString = "hello".split("");
     // ["h", "e", "l", "l", "o"]
 
     // Step 2. Use the reverse() method to reverse the new created array
-    var reverseArray = splitString.reverse(); // var reverseArray = ["h", "e", "l", "l", "o"].reverse();
+    let reverseArray = splitString.reverse(); // var reverseArray = ["h", "e", "l", "l", "o"].reverse();
     // ["o", "l", "l", "e", "h"]
 
     // Step 3. Use the join() method to join all elements of the array into a string
@@ -41,11 +44,20 @@ function reversal() {
 
 // return the characters in alphabetical order, and outputs the result in the DOM, below the text input.
 function alphabits() {
+    alphaOrder = testString.split('').sort().join('');
     palindrome();
 }
 
 // determine whether the string is a palindrome. If it is, display the text "Your string is a palidrome" in the DOM, below the text input.
 function palindrome() {
+    // to lowercase
+    var lowerTestString = testString.toLowerCase();
+    var lowerJoinArray = joinArray.toLowerCase();
+    if (lowerTestString === lowerJoinArray) {
+        palinYesOrNo = "That is a palindrome!";
+    } else {
+        palinYesOrNo = "Not a palindrome.";
+    }
     writeToHTML();
 }
 
@@ -54,9 +66,11 @@ function writeToHTML() {
     resultsDiv.innerHTML = '';
     resultsDiv.innerHTML = `
         <p>Reversal: ${joinArray}</p>
-        <p>Alphabits: </p>
-        <p>palindrone: </p>`
+        <p>Alphabits: ${alphaOrder}</p>
+        <p>Palindrome: ${palinYesOrNo}</p>`
 }
+
+
 ///////////////////////////
 ///   Event Listeners   ///
 ///////////////////////////
@@ -72,7 +86,3 @@ inputBox.addEventListener('keydown', function(e) {
 // Listen for button press
 let submitButton = document.querySelector('#subButton');
 submitButton.addEventListener('click', getInput);
-
-// reversal(testString);
-// alphabits(testString);
-// palindrome(testString);
