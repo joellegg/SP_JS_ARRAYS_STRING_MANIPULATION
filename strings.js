629c5700569221ae44d32b7b8dd4e2fec2be4e2e
joellegg <joellegg@bellsouth.net>
2016-12-23 09:29:42 -0600

    removed form element, added event listener to return key to get input value

--- strings.js
+++ strings.js
@@ -1,3 +1,18 @@
+////////////////////////////
+///   Global Variables   ///
+////////////////////////////
+
+let testString;
+
+/////////////////////
+///   Functions   ///
+/////////////////////
+
+function getInput() {
+    testString = document.querySelector('#stringInput').value;
+    console.log('testString', testString);
+}
+
 function reversal() {
 
 }
@@ -10,7 +25,18 @@ function palindrome() {
 
 }
 
-var testString = "";
-reversal(testString);
-alphabits(testString);
-palindrome(testString);
+///////////////////////////
+///   Event Listeners   ///
+///////////////////////////
+
+// Listen for enter key or button press
+var inputBox = document.querySelector('#stringInput');
+inputBox.addEventListener('keydown', function(e) {
+    if (e.key === 'Enter') {
+        getInput();
+    }
+})
+
+// reversal(testString);
+// alphabits(testString);
+// palindrome(testString);
